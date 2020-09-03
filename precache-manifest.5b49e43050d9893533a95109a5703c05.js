67abc2e06c98ae5ae5bf3af30d9f2279a055abb4
SergiuBic <wikin_n@yahoo.com>
2020-09-04 01:37:06 +0300

    Updates

---
+++ precache-manifest.5b49e43050d9893533a95109a5703c05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b39f412bd9f510b3afd7",
+    "url": "/ReactSimpleToDo/static/css/main.5b45b91c.chunk.css"
+  },
+  {
+    "revision": "b39f412bd9f510b3afd7",
+    "url": "/ReactSimpleToDo/static/js/main.e60588c1.chunk.js"
+  },
+  {
+    "revision": "7c4d6f857c8bd486edda",
+    "url": "/ReactSimpleToDo/static/js/runtime~main.feb63727.js"
+  },
+  {
+    "revision": "48a4102fde9262ffbe16",
+    "url": "/ReactSimpleToDo/static/js/2.65179569.chunk.js"
+  },
+  {
+    "revision": "437d2592f530b87498088a8ae5647fe9",
+    "url": "/ReactSimpleToDo/index.html"
+  }
+];
